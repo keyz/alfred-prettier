f023dd1e449f7c3a021e777385074c73c4e5f7f1
Keyan Zhang <root@keyanzhang.com>
2019-03-08 02:24:24 -0800

    deps

--- __tests__/index-test.js
+++ __tests__/index-test.js
@@ -1,40 +1,44 @@
 const alfyTest = require('alfy-test');
 
+function formatOutput(input, output, flowStrippedOutput) {
+  return [
+    {
+      arg: output,
+      subtitle: `Prettified "${input}"`,
+      text: {
+        copy: output,
+        largetype: output,
+      },
+      title: output,
+    },
+    {
+      arg: flowStrippedOutput,
+      icon: {
+        path: 'flow-logo.png',
+      },
+      subtitle: `Flow stripped & Prettified "${input}"`,
+      text: {
+        copy: flowStrippedOutput,
+        largetype: flowStrippedOutput,
+      },
+      title: flowStrippedOutput,
+    },
+  ];
+}
+
 test('it works', async () => {
   const input = `const foo: {[key: string]:number} = {james:12,lucy: 24,ming:235};`;
 
   const alfy = alfyTest();
   const result = await alfy(input);
 
-  expect(result).toEqual([
-    {
-      arg:
-        'const foo: {[key: string]: number} = {james: 12, lucy: 24, ming: 235};\n',
-      subtitle:
-        'Prettified "const foo: {[key: string]:number} = {james:12,lucy: 24,ming:235};"',
-      text: {
-        copy:
-          'const foo: {[key: string]: number} = {james: 12, lucy: 24, ming: 235};\n',
-        largetype:
-          'const foo: {[key: string]: number} = {james: 12, lucy: 24, ming: 235};\n',
-      },
-      title:
-        'const foo: {[key: string]: number} = {james: 12, lucy: 24, ming: 235};\n',
-    },
-    {
-      arg: 'const foo = {james: 12, lucy: 24, ming: 235};\n',
-      icon: {
-        path: 'flow-logo.png',
-      },
-      subtitle:
-        'Flow stripped & Prettified "const foo: {[key: string]:number} = {james:12,lucy: 24,ming:235};"',
-      text: {
-        copy: 'const foo = {james: 12, lucy: 24, ming: 235};\n',
-        largetype: 'const foo = {james: 12, lucy: 24, ming: 235};\n',
-      },
-      title: 'const foo = {james: 12, lucy: 24, ming: 235};\n',
-    },
-  ]);
+  expect(result).toEqual(
+    formatOutput(
+      input,
+      'const foo: {[key: string]: number} = {james: 12, lucy: 24, ming: 235};\n',
+      'const foo = {james: 12, lucy: 24, ming: 235};\n',
+    ),
+  );
 });
 
 test('it reports errors', async () => {
